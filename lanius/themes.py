5715dc5e938725311137aec6c49ab5d0eadf3e12
TomCrypto <thomas.beneteau@yahoo.fr>
2015-08-31 20:55:12 +1200

    Convert all embedded unicode chars to unicode escape sequences.

--- lanius/themes.py
+++ lanius/themes.py
@@ -1,63 +1,61 @@
-# coding: utf8
-
 from munch import munchify
 from six import u
 
 DefaultTheme = {
     'hr': munchify({
-        'head': u'─',
-        'rule': u'─',
-        'last': u'─',
+        'head': u('\u2015'),
+        'rule': u('\u2015'),
+        'last': u('\u2015'),
     }),
     'div': munchify({
         'margin': u(' '),
-        'links': '[{index}] \033[3;4m{href}\033[0m',
+        'links': u('[{index}] \033[3;4m{href}\033[0m'),
     }),
     'blockquote': munchify({
-        'margin': u('\033[33;1m█\033[0m '),
+        'margin': u('\033[33;1m\u2588\033[0m '),
     }),
     'plain': munchify({
         'render': lambda text: text,
     }),
     'strong': munchify({
-        'render': lambda text: '\033[1m{0}\033[0m'.format(text),
+        'render': lambda text: u('\033[1m{0}\033[0m').format(text),
     }),
     'em': munchify({
-        'render': lambda text: '\033[3m{0}\033[0m'.format(text),
+        'render': lambda text: u('\033[3m{0}\033[0m').format(text),
     }),
     'a': munchify({
-        'render': lambda i, text: '\033[3;4m{0}\033[0m [{1}]'.format(i, text)
+        'render': lambda i, text: u('\033[3;4m{0}\033[0m [{1}]').format(i, text)
     }),
     'code': munchify({
         'inline': {
-            'render': lambda text: '\033[37;3m{0}\033[0m'.format(text),
+            'render': lambda text: u('\033[37;3m{0}\033[0m').format(text),
         },
         'block': {
             'highlight': True,
         },
     }),
     'h1': munchify({
-        'fill': u'▄',
+        'fill': u('\u2584'),
     }),
     'h2': munchify({
-        'prefix': u'\033[1m§\033[0m \033[4;3;1m',
-        'suffix': u'\033[0m',
+        'prefix': u('\033[1m\u00A7\033[0m \033[4;3;1m'),
+        'suffix': u('\033[0m'),
     }),
     'h3': munchify({
-        'prefix': u'\033[1m§\033[0m \033[4;1m',
-        'suffix': u'\033[0m',
+        'prefix': u('\033[1m\u00A7\033[0m \033[4;1m'),
+        'suffix': u('\033[0m'),
     }),
     'h4': munchify({
-        'prefix': u'\033[1m§\033[0m \033[1m',
-        'suffix': u'\033[0m',
+        'prefix': u('\033[1m\u00A7\033[0m \033[1m'),
+        'suffix': u('\033[0m'),
     }),
     'h5': munchify({
-        'prefix': u'\033[1m§\033[0m \033[4m',
-        'suffix': u'\033[0m',
+        'prefix': u('\033[1m\u00A7\033[0m \033[4m'),
+        'suffix': u('\033[0m'),
     }),
     'h6': munchify({
-        'prefix': u'\033[1m§\033[0m ',
-        'suffix': '',
+        'prefix': u('\033[1m\u00A7\033[0m '),
+        'suffix': u(''),
     }),
     'ol': munchify({
         'margin': {
@@ -67,7 +65,7 @@ DefaultTheme = {
     }),
     'ul': munchify({
         'margin': {
-            'head': u('• '),
+            'head': u('\u2022 '),
             'tail': lambda length: u(' ') * length,
         },
     }),
@@ -84,80 +82,80 @@ DefaultTheme = {
 
 PlainTheme = {
     'hr': munchify({
-        'head': '-',
-        'rule': '-',
-        'last': '-',
+        'head': u('-'),
+        'rule': u('-'),
+        'last': u('-'),
     }),
     'div': munchify({
-        'margin': ' ',
-        'links': '[{index}] {href}',
+        'margin': u(' '),
+        'links': u('[{index}] {href}'),
     }),
     'blockquote': munchify({
-        'margin': '> ',
+        'margin': u('> '),
     }),
     'plain': munchify({
         'render': lambda text: text,
     }),
     'strong': munchify({
-        'render': lambda text: '**{0}**'.format(text),
+        'render': lambda text: u('**{0}**').format(text),
     }),
     'em': munchify({
-        'render': lambda text: '*{0}*'.format(text),
+        'render': lambda text: u('*{0}*').format(text),
     }),
     'a': munchify({
-        'render': lambda idx, text: '<{0}> [{1}]'.format(idx, text),
+        'render': lambda idx, text: u('<{0}> [{1}]').format(idx, text),
     }),
     'code': munchify({
         'inline': {
-            'render': lambda text: '`{0}`'.format(text),
+            'render': lambda text: u('`{0}`').format(text),
         },
         'block': {
             'highlight': False,
         },
     }),
     'h1': munchify({
-        'fill': '=',
+        'fill': u('='),
     }),
     'h2': munchify({
-        'prefix': '§ ',
-        'suffix': '',
+        'prefix': u('\u00A7 '),
+        'suffix': u(''),
     }),
     'h3': munchify({
-        'prefix': '§ ',
-        'suffix': '',
+        'prefix': u('\u00A7 '),
+        'suffix': u(''),
     }),
     'h4': munchify({
-        'prefix': '§ ',
-        'suffix': '',
+        'prefix': u('\u00A7 '),
+        'suffix': u(''),
     }),
     'h5': munchify({
-        'prefix': '§ ',
-        'suffix': '',
+        'prefix': u('\u00A7 '),
+        'suffix': u(''),
     }),
     'h6': munchify({
-        'prefix': '§ ',
-        'suffix': '',
+        'prefix': u('\u00A7 '),
+        'suffix': u(''),
     }),
     'ol': munchify({
         'margin': {
-            'head': lambda index: '{0}. '.format(index),
-            'tail': lambda length: ' ' * length,
+            'head': lambda index: u('{0}. ').format(index),
+            'tail': lambda length: u(' ') * length,
         },
     }),
     'ul': munchify({
         'margin': {
-            'head': '- ',
-            'tail': lambda length: ' ' * length,
+            'head': u('- '),
+            'tail': lambda length: u(' ') * length,
         },
     }),
     'pre': munchify({
-        'margin': '  ',
+        'margin': u('  '),
     }),
     'li': munchify({
-        'margin': '',
+        'margin': u(''),
     }),
     'p': munchify({
-        'margin': '',
+        'margin': u(''),
     }),
 }
 
